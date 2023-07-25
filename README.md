d64e2fd8299d0993fc492590ff27a17219f874dd
Sayib <sayib.roziq2@gmail.com>
2023-07-25 13:21:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Larative
+First look Laravel App with NativePHP
+
+- PHP 8.2
+- Laravel 10.10
+- NativePHP Alpha 0.1.1
+
+```
+$ composer install
+$ php artisan native:serve
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Larative</title>
+</head>
+
+<body>
+    <center>
+        <h1>
+            Welcome to Larative Desktop Native App
+        </h1>
+        <a href="https://github.com/msayib" target="_blank">Github</a>
+    </center>
+</body>
+
+</html>
